4b60958db22bc9bbd5c4225d42aba07b19087c16
Pranay Kumar Reddy Pothula <pranaykumarreddypothula@gmail.com>
2026-02-05 10:54:06 +0530

    Initial commit - CineKremlin movie app

---
+++ script.js
@@ -0,0 +1,40 @@
+let input = document.getElementById("movie");
+let button = document.getElementById("searchBtn");
+let result = document.getElementById("result");
+button.addEventListener("click", getMovie);
+input.addEventListener("keydown", function(event){
+    if(event.key === "Enter"){
+        getMovie();
+    }
+});
+function getMovie(){
+    let movie = input.value.trim();
+    if(movie === ""){
+        result.innerText = "Please enter a movie name";
+        return
+    }
+    let apiKey = "217430e3";
+    let url = `https://www.omdbapi.com/?t=${movie}&apikey=${apiKey}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            if(data.Response === "False"){
+                result.innerText = "Movie not found";
+                return;
+            }
+            result.innerHTML = `<h3>${data.Title} (${data.Year})</h3>
+                <img src="${data.Poster}" width="240">
+                <p><b>IMDB:</b> ${data.imdbRating}</p>
+                <p>${data.Plot}</p>
+                <button onclick="watchTrailer('${data.Title}')">
+                    🎬 Watch Trailer
+                </button>`;
+        })
+        .catch(() => {
+            result.innerText = "Error fetching movie!!!";
+        });
+}
+       function watchTrailer(movieName){
+    let url = `https://www.youtube.com/results?search_query=${movieName}+trailer`;
+    window.open(url, "_blank");
+}
